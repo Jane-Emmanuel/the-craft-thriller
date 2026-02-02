4e50c2f680f4d0459f59bb9a1a093a92e88adab9
Jane <janeemmanuel247@gmail.com>
2026-02-01 22:54:24 -0800

    Delete service worker implementation in sw.js
    Remove service worker caching logic.

--- sw.js
+++ sw.js
@@ -1,19 +1,1 @@
-const CACHE = "craft-thriller-v1";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ])
-    )
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
